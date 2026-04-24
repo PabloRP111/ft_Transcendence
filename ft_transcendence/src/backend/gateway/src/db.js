2542ee68ff0f2380749c3d650871b956b8d6cd0e
aleamoros <amorosale98@gmail.com>
2026-04-24 19:58:53 +0200

    cleaned up hardcoding of postgres varaibles

--- ft_transcendence/src/backend/gateway/src/db.js
+++ ft_transcendence/src/backend/gateway/src/db.js
@@ -3,11 +3,11 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  host: "postgres",
-  user: "transcendence",
-  password: "transcendence",
-  database: "transcendence",
-  port: 5432
+  host: process.env.POSTGRES_HOST || "postgres",
+  user: process.env.POSTGRES_USER || "transcendence",
+  password: process.env.POSTGRES_PASSWORD || "transcendence",
+  database: process.env.POSTGRES_DB || "transcendence",
+  port: Number(process.env.POSTGRES_PORT) || 5432
 });
 
 pool.on("connect", () => {
